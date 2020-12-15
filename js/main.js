7c90963d102a5bc313cc71c9e71db964fafe046f
pedro7161 <pedro.ramos9@hotmail.com>
2020-12-15 13:33:20 +0000

    ediçao em aula

--- js/main.js
+++ js/main.js
@@ -25,8 +25,12 @@ $(document).ready(function (){
         //Se o menu estiver fechado abrir o menu caso contrario fechar
         if( $(".topnav").css("display") == "none" ){
             $(".topnav").css("display","block");
+            $(".icons i").removeClass("fa-bars");
+            $(".icons i").addClass("fa-times");
         }else{
             $(".topnav").css("display","none");
+            $(".icons i").removeClass("fa-times");
+            $(".icons i").addClass("fa-bars");
         }
     });
 
